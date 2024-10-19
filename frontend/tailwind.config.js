53f0e87ccb257c826923779ba9326dc9b085eebd
Kerai Ramesh <rkerai34@gmail.com>
2024-10-19 11:22:15 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      primary: '#ED9455',
+      secondary: '#FFBB70',
+      ternary: '#FFEC9E',
+      quad: '#FFFBDA',
+    },
+  },
+  plugins: [],
+}
